5afd43ad7051c90c50fdb9a1f8f4e8ce4be21930
Dan <daniel@missud.net>
2020-09-03 15:23:29 +0200

    Revert "Passage au dernière version suite correction env de dev en interne Valmy"
    This reverts commit 8a1128c4

--- exposition/src/main/java/org/yde/ydeapp/exposition/application/ApplicationResource.java
+++ exposition/src/main/java/org/yde/ydeapp/exposition/application/ApplicationResource.java
@@ -16,7 +16,7 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/V1")
+@RequestMapping("/api")
 public class ApplicationResource {
 
     @Autowired
