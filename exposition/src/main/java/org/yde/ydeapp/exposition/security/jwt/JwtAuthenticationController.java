abf521976e4cbde2d8c6a5b3f4e7e5d4a55fa2a8
dmissud <daniel@missud.net>
2020-10-08 14:56:54 +0200

    Finalisation du users Service avec service applicatif

--- exposition/src/main/java/org/yde/ydeapp/exposition/security/jwt/JwtAuthenticationController.java
+++ exposition/src/main/java/org/yde/ydeapp/exposition/security/jwt/JwtAuthenticationController.java
@@ -7,43 +7,46 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.yde.ydeapp.domain.out.BusinessException;
 
 @RestController
 @CrossOrigin
 public class JwtAuthenticationController {
 
-	@Autowired
-	private AuthenticationManager authenticationManager;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-	@Autowired
-	private JwtTokenManager jwtTokenUtil;
+    @Autowired
+    private JwtTokenManager jwtTokenUtil;
 
-	@Autowired
-	private CustomUserDetailsService userDetailsService;
+    @Autowired
+    private YdeAppUserDetailsService userDetailsService;
 
-	@PostMapping(value = "/authenticate")
-	public ResponseEntity<JwtResponse> createAuthenticationToken(@RequestBody final JwtRequest authenticationRequest)
-			throws Exception {
+    @PostMapping(value = "/authenticate")
+    public ResponseEntity<JwtResponse> createAuthenticationToken(@RequestBody final JwtRequest authenticationRequest) {
 
-		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 
-		final UserDetails userDetails = userDetailsService
-				.loadUserByUsername(authenticationRequest.getUsername());
+        final UserDetails userDetails = userDetailsService
+            .loadUserByUsername(authenticationRequest.getUsername());
 
-		final String token = jwtTokenUtil.generateToken(userDetails);
+        final String token = jwtTokenUtil.generateToken(userDetails);
 
-		return ResponseEntity.ok(new JwtResponse(token, userDetails.getAuthorities()));
-	}
+        return ResponseEntity.ok(new JwtResponse(token, userDetails.getAuthorities()));
+    }
 
-	private void authenticate(final String username, final String password) throws Exception {
-		try {
-			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-		} catch (final DisabledException e) {
-			throw new Exception("USER_DISABLED", e);
-		} catch (final BadCredentialsException e) {
-			throw new Exception("INVALID_CREDENTIALS", e);
-		}
-	}
+    private void authenticate(final String username, final String password) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (final DisabledException e) {
+            throw new BusinessException("USER_DISABLED", e);
+        } catch (final BadCredentialsException e) {
+            throw new BusinessException("INVALID_CREDENTIALS", e);
+        }
+    }
 
 }
---
+++ exposition/src/main/java/org/yde/ydeapp/exposition/security/jwt/YdeAppUserDetailsService.java
@@ -0,0 +1,92 @@
+package org.yde.ydeapp.exposition.security.jwt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yde.ydeapp.application.in.GetUserQuery;
+import org.yde.ydeapp.domain.application.User;
+import org.yde.ydeapp.domain.out.EntityNotFound;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class YdeAppUserDetailsService implements UserDetailsService {
+
+    public static final String ADMIN = "admin";
+    public static final String USER = "user";
+
+    @Autowired
+    GetUserQuery getUserQuery;
+
+    @Autowired
+    public PasswordEncoder passwordEncoder;
+
+    private static Logger log = LoggerFactory.getLogger(YdeAppUserDetailsService.class);
+    final String[] userRoles = {"ROLE_USER"};
+    final String[] adminRoles = {"ROLE_ADMIN"};
+
+    // ici je transforme mes objets du domain en objets internes de Spring Security
+    @Override
+    public UserDetails loadUserByUsername(final String uid) {
+        User user;
+        UserDetails userDetails;
+        try {
+            user = getUserQuery.getUserByUid(uid);
+            log.debug("Identification of {}", uid);
+            userDetails = new UserDetails() {
+                @Override
+                public Collection<? extends GrantedAuthority> getAuthorities() {
+
+                    return user.getRoles()
+                        .stream()
+                        .map(role -> (GrantedAuthority) role::toString)
+                        .collect(Collectors.toList());
+                }
+
+                @Override
+                public String getPassword() {
+                    return user.getPassword();
+                }
+
+                @Override
+                public String getUsername() {
+                    return user.getUid();
+                }
+
+                @Override
+                public boolean isAccountNonExpired() {
+                    return false;
+                }
+
+                @Override
+                public boolean isAccountNonLocked() {
+                    return true;
+                }
+
+                @Override
+                public boolean isCredentialsNonExpired() {
+                    return true;
+                }
+
+                @Override
+                public boolean isEnabled() {
+                    return true;
+                }
+            };
+            return userDetails;
+
+        } catch (EntityNotFound entityNotFound) {
+            log.debug("User {} not exitent", uid);
+            throw new UsernameNotFoundException(entityNotFound.getMessage());
+        }
+    }
+}
