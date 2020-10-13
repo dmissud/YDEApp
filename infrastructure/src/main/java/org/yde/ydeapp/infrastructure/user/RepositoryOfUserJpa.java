c339e3137d45dd437a86395ef87e4b8a15e5aaa8
dmissud <daniel@missud.net>
2020-10-13 10:00:25 +0200

    Correctif bug creation UserDesc

--- infrastructure/src/main/java/org/yde/ydeapp/infrastructure/user/RepositoryOfUserJpa.java
+++ infrastructure/src/main/java/org/yde/ydeapp/infrastructure/user/RepositoryOfUserJpa.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface RepositoryOfUserJpa extends JpaRepository<UserEntity, Integer> {
 
     UserEntity findByUid(String uid);
-    @Query(value = "SELECT new org.yde.ydeapp.domain.user.UserDesc(user.uid, user.firstName, user.lastName, user.roles) FROM UserEntity user")
+    @Query(value = "SELECT new org.yde.ydeapp.domain.user.UserDesc(user.firstName, user.lastName, user.uid, user.roles) FROM UserEntity user")
     List<UserDesc> retrieveAllUsers();
 
 }
