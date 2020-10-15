8f5468ae9bddaf8e2172471176fb3ed8462fd243
yannick boscher <yannickhanh@gmail.com>
2020-10-15 11:29:21 +0200

    revu des tests sur l'expo sur les notes suite au modif sécurité

--- exposition/src/test/java/org/yde/ydeapp/exposition/note/NoteResourceTest.java
+++ exposition/src/test/java/org/yde/ydeapp/exposition/note/NoteResourceTest.java
@@ -10,14 +10,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.yde.ydeapp.application.in.application.ApplicationQuery;
 import org.yde.ydeapp.application.in.application.GetNoteQuery;
 import org.yde.ydeapp.application.in.application.ReferenceNoteUseCase;
+import org.yde.ydeapp.application.in.application.UpdateNoteUseCase;
+import org.yde.ydeapp.application.in.user.GetUserQuery;
 import org.yde.ydeapp.domain.application.Note;
 import org.yde.ydeapp.domain.out.EntityNotFound;
+import org.yde.ydeapp.domain.user.User;
 import org.yde.ydeapp.exposition.application.NoteResource;
+import org.yde.ydeapp.exposition.security.jwt.JwtAuthenticationEntryPoint;
+import org.yde.ydeapp.exposition.security.jwt.JwtTokenManager;
+import org.yde.ydeapp.exposition.security.jwt.YdeAppUserDetailsService;
 
 
 import java.time.LocalDate;
@@ -28,7 +36,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(NoteResource.class)
+@WebMvcTest({NoteResource.class, YdeAppUserDetailsService.class})
 class NoteResourceTest {
 
     public static final String CODE_APPLICATION = "AP00001";
@@ -50,16 +58,32 @@ class NoteResourceTest {
     private ReferenceNoteUseCase referenceNoteUseCase;
 
     @MockBean
+    private UpdateNoteUseCase updateNoteUseCase;
+
+    @MockBean
+    private GetUserQuery getUserQuery;
+
+
+    @MockBean
+    JwtTokenManager jwtTokenManager;
+
+    @MockBean
+    JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+
+
+    @MockBean
     private GetNoteQuery getNoteQuery;
 
     private ReferenceNoteUseCase.ReferenceNoteCmd referenceNoteCmd;
 
     private List<Note> notes;
     Note noteInit = new Note(NOTE_TITLE_FIRST, NOTE_CONTENT_FIRST, NOTE_CREATION_DATE_FIRST);
+    private User user;
 
     @BeforeEach
     void setup() {
-
+        user = new User("Admin", "UserAdmin", "FromAdmin", "$2a$10$hKDVYxLefVHV/vtuPhWD3OigtRyOykRLDdUAp80Z1crSoS1lFqaFS", 6);
         Note note2nd = new Note(NOTE_TITLE_SECOND, NOTE_CONTENT_SECOND, NOTE_CREATION_DATE_SECOND);
         notes = new ArrayList<>();
         notes.add(noteInit);
@@ -70,6 +94,7 @@ class NoteResourceTest {
 
     @Test
     @DisplayName("Retrieve a notes-list from an existing application")
+    @WithMockUser(roles={"USER"})
     void testRetrieveAllNotesFromExistingApplication() throws Exception {
         // Given
         Mockito
@@ -78,7 +103,7 @@ class NoteResourceTest {
 
         mockMvc
             // When
-            .perform(MockMvcRequestBuilders.get("/api/applications/" + CODE_APPLICATION + "/notes")
+            .perform(MockMvcRequestBuilders.get("/api/V1/applications/" + CODE_APPLICATION + "/notes")
                 .accept(MediaType.APPLICATION_JSON))
             // Then
             .andExpect(status().isOk());
@@ -86,6 +111,7 @@ class NoteResourceTest {
 
     @Test
     @DisplayName("Retrieve a defined note from an existing application")
+    @WithMockUser(roles={"USER"})
     void testRetrieveNoteByTitleFromExistingApplication() throws Exception {
         // Given
         Mockito
@@ -94,7 +120,7 @@ class NoteResourceTest {
 
         mockMvc
             // When
-            .perform(MockMvcRequestBuilders.get("/api/applications/" + CODE_APPLICATION + "/notes/" + NOTE_TITLE_FIRST)
+            .perform(MockMvcRequestBuilders.get("/api/V1/applications/" + CODE_APPLICATION + "/notes/" + NOTE_TITLE_FIRST)
                 .accept(MediaType.APPLICATION_JSON))
             // Then
             .andExpect(status().isOk());
@@ -102,6 +128,7 @@ class NoteResourceTest {
 
     @Test
     @DisplayName("Reference a new note to an existing application")
+    @WithMockUser(roles={"USER"})
     void testReferenceNewNoteToExistingApplication() throws Exception {
         // Given
         Mockito
@@ -112,7 +139,7 @@ class NoteResourceTest {
         mockMvc
             // When
             .perform(MockMvcRequestBuilders
-                .post("/api/applications/" + CODE_APPLICATION + "/notes")
+                .post("/api/V1/applications/" + CODE_APPLICATION + "/notes")
                 .content(json)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
@@ -122,13 +149,14 @@ class NoteResourceTest {
 
     @Test
     @DisplayName("Delete a note from an existing application")
+    @WithMockUser(roles={"USER"})
     void testDeleteNoteFromExistingApplication() throws Exception {
         // Given
         // rien a mocker la méthode est en void, il faut juste vérifier qu'elle est appellée
         mockMvc
             // When
             .perform(MockMvcRequestBuilders
-                .delete("/api/applications/" + CODE_APPLICATION + "/notes/" + NOTE_TITLE_FIRST)
+                .delete("/api/V1/applications/" + CODE_APPLICATION + "/notes/" + NOTE_TITLE_FIRST)
                 .accept(MediaType.APPLICATION_JSON))
             // Then
             .andExpect(status().isOk());
@@ -138,6 +166,7 @@ class NoteResourceTest {
 
     @Test
     @DisplayName("Try to Delete a unknow note from an existing application and got a 404 error")
+    @WithMockUser(roles={"USER"})
     void testDeleteUnknowNoteFromExistingApplication() throws Exception {
         // Given
         Mockito
@@ -147,9 +176,25 @@ class NoteResourceTest {
         mockMvc
             // When
             .perform(MockMvcRequestBuilders
-                .delete("/api/applications/" + CODE_APPLICATION + "/notes/" + NOTE_TITLE_FIRST)
+                .delete("/api/V1/applications/" + CODE_APPLICATION + "/notes/" + NOTE_TITLE_FIRST)
                 .accept(MediaType.APPLICATION_JSON))
             // Then
             .andExpect(status().isNotFound());
+    }
+    @Test
+    @DisplayName("Delete a note from an existing application with no role")
+    @WithMockUser(roles={""})
+    void testDeleteNoteFromExistingApplicationWithNoRole() throws Exception {
+        // Given
+        // rien a mocker la méthode est en void, il faut juste vérifier qu'elle est appellée
+
+        mockMvc
+                // When
+                .perform(MockMvcRequestBuilders
+                        .delete("/api/V1/applications/" + CODE_APPLICATION + "/notes/" + NOTE_TITLE_FIRST)
+                        .accept(MediaType.APPLICATION_JSON))
+                // Then
+                .andExpect(status().isForbidden());
+
     }
 }
